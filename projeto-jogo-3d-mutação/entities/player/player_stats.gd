0a583d9fdd70d6c4a961f06cefb98cfa60aba503
PscheidtLucas <163616249+PscheidtLucas@users.noreply.github.com>
2025-10-27 12:02:33 -0300

    small att

--- projeto-jogo-3d-mutação/entities/player/player_stats.gd
+++ projeto-jogo-3d-mutação/entities/player/player_stats.gd
@@ -1,3 +1,13 @@
 class_name PlayerStats extends Resource
 
 @export var max_health: float = 100.0
+
+@export var health_regen_amount: float = 0.0
+@export var lifesteal_amount: float = 0.0
+
+@export var damage_increase: float = 0.0
+@export var speed_increase: float = 0.0
+@export var crit_chance_increase: float = 0.0
+@export var crit_damage_increase: float = 0.0 
+@export var fire_rate_increase: float = 0.0
+@export var collect_area_increase: float = 0.0
