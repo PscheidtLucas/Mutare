d5cb562ced508b82dc5e7a85a0474ce4df937105
PscheidtLucas <163616249+PscheidtLucas@users.noreply.github.com>
2025-10-29 02:11:56 -0300

    bomba n trava o jogo mais

---
+++ projeto-jogo-3d-mutação/bullets/bomb_explode_one_frame.gd
@@ -0,0 +1,12 @@
+extends Bomb
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	
+	debris.emitting = true
+	smoke.emitting = true
+	fire.emitting = true
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
