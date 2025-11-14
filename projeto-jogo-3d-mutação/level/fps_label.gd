9421a7c3a6c61bcab79ee39f059a9a36f5c7aa73
PscheidtLucas <163616249+PscheidtLucas@users.noreply.github.com>
2025-11-14 01:13:39 -0300

    atualizacao da cor da grama e teste com otimizacao
    Otimizar a grama nao compensa, de qualquer forma (sendo em chunks ou nao) o fps praticamente nao muda, só nao testei com um visibleOnScreen notifier para dar hide nos nós q n apareçam na tela, mas acredito q nao mude mt coisa a ponto do jogo ser rodavel no web export pq mesmo desativando visualmente todas as gramas e testando na web, ainda ficou lagado (acredito q seja por conta da navegacao dos inimigos).

--- projeto-jogo-3d-mutação/level/fps_label.gd
+++ projeto-jogo-3d-mutação/level/fps_label.gd
@@ -1,5 +1,11 @@
 extends Label
 
+#func _ready() -> void:
+	#if Engine.is_editor_hint():
+		#pass
+	#else:
+		#self.queue_free()
+		
 
 func _process(delta: float) -> void:
 	text = "FPS: " + str(Engine.get_frames_per_second())
